55fe0439f90b9a88083acac728c0faceac874c4f
mlluiz21 <mlluiz21@gmail.com>
2016-10-27 09:15:47 -0200

    falta finalizar o VeiculoDAO

--- Desktop/ProjetoStopCar/ProjetoStopCar/src/modelo/dao/VeiculoDAO.java
+++ Desktop/ProjetoStopCar/ProjetoStopCar/src/modelo/dao/VeiculoDAO.java
@@ -1,5 +1,29 @@
 package modelo.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.dominio.Veiculo;
+
 public class VeiculoDAO {
+	
+	private EntityManager manager = null;
+	
+	public VeiculoDAO(){
+		
+		super();
+		
+		EntityManagerFactory criandoMinhaFabrica = Persistence.createEntityManagerFactory("ProjetoBancoStopCar");
+		
+		this.manager = criandoMinhaFabrica.createEntityManager();
+	}
+	
+	public void incluirVeiculo(Veiculo veiculo){
+		
+		this.manager.getTransaction().begin();
+		
+		
+	}
 
 }
