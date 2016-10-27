3d025946f5b88c94c06bc7d3eb1f5f8604815b03
katia.souza <katia.souza@LUC00536291.ibge.gov.br>
2016-10-27 17:06:22 -0200

    VeiculoDAO faltando ainda...

--- Desktop/ProjetoStopCar/ProjetoStopCar/src/modelo/dao/VeiculoDAO.java
+++ Desktop/ProjetoStopCar/ProjetoStopCar/src/modelo/dao/VeiculoDAO.java
@@ -21,9 +21,53 @@ public class VeiculoDAO {
 	
 	public void incluirVeiculo(Veiculo veiculo){
 		
+		//INICIAR TRANSAÇÃO PARA PERSISTÊNCIA NO BANCO.
 		this.manager.getTransaction().begin();
 		
+		//SALVAR OS DADOS QUE ESTÃO NOS OBJETOS.
+		this.manager.merge(veiculo);
 		
+		//FAZER A PERSISTÊNCIA ENVIANDOS COMANDOS PARA O BANCO.
+		this.manager.flush();
+		
+		//CONFIRMA TODA TRANSAÇÃO COM O BANCO.
+		this.manager.getTransaction().commit();		
+	}
+	
+	public void alterarVeiculo(Veiculo veiculo){
+		
+		//INICIAR TRANSAÇÃO PARA PERSISTÊNCIA NO BANCO.
+		this.manager.getTransaction().begin();
+		
+		//SALVAR OS DADOS QUE ESTÃO NOS OBJETOS.
+		this.manager.merge(veiculo);
+		
+		//FAZER A PERSISTÊNCIA ENVIANDOS COMANDOS PARA O BANCO.
+		this.manager.flush();
+		
+		//CONFIRMA TODA TRANSAÇÃO COM O BANCO.
+		this.manager.getTransaction().commit();
+	}
+	
+	public void excluirVeiculo(Veiculo veiculo){
+		
+		//INICIAR A TRANSAÇÃO PARA PERSISTÊNCIA NO BANCO.
+		this.manager.getTransaction().begin();
+		
+		//FAZ A REMOÇÃO DOS DADOS.
+		this.manager.remove(veiculo);
+		
+		//ENVIANDOS COMANDOS PARA O BANCO PARA REMOÇÃO DOS DADOS.
+		this.manager.flush();
+		
+		//CONFIRMA TODA TRANSAÇÃO COM O BANCO.
+		this.manager.getTransaction().commit();
+	}
+	
+	public void consultarVeiculo(Veiculo veiculo){
+		
+		this.manager.getTransaction().begin();
+				
 	}
 
 }
